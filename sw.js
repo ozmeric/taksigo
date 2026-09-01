345f5a95886aecdbc543ac89f5b01820dad9a31f
Weblina <33263939+ozmeric@users.noreply.github.com>
2026-08-31 21:22:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "taksigo-v1";
+const CACHE = "taksigo-v3";
 const SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
